78b30ece1d16f44725f2a4daca60eca2b2ce9170
Mayra Amaral <mayracunha18@gmail.com>
2024-12-30 20:56:43 -0300

    feat: initial project

---
+++ src/main/java/techchallenge/fiap/TechChallengeFase01Application.java
@@ -0,0 +1,13 @@
+package techchallenge.fiap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeFase01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechChallengeFase01Application.class, args);
+    }
+
+}
---
+++ src/test/java/techchallenge/fiap/TechChallengeFase01ApplicationTests.java
@@ -0,0 +1,13 @@
+package techchallenge.fiap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeFase01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
