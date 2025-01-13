1eb21a440d630e3417921828667042b31c4e4af4
Mayra Amaral <mayracunha18@gmail.com>
2025-01-12 23:03:04 -0300

    refactor: atualiza GlobalExceptionHandler para utilizar record em vez de classe

--- src/main/java/techchallenge/fiap/utils/handlers/GlobalExceptionHandler.java
+++ src/main/java/techchallenge/fiap/utils/handlers/GlobalExceptionHandler.java
@@ -7,8 +7,9 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import techchallenge.fiap.utils.errors.ErrorResponseDTO;
+import techchallenge.fiap.utils.exceptions.DadosIncorretosException;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -17,14 +18,15 @@ import java.util.stream.Collectors;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponseDTO> handleValidationExceptions(MethodArgumentNotValidException ex, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponseWithErrorsMapDTO> handleValidationExceptions(MethodArgumentNotValidException ex, HttpServletRequest request) {
         Map<String, String> errors = ex.getBindingResult().getFieldErrors().stream()
             .collect(Collectors.toMap(
                 FieldError::getField,
                 error -> Optional.ofNullable(error.getDefaultMessage())
                     .orElse("Mensagem de erro não disponível")));
 
-        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+        ErrorResponseWithErrorsMapDTO errorResponse = new ErrorResponseWithErrorsMapDTO(
+            LocalDateTime.now().toString(),
             HttpStatus.BAD_REQUEST.value(),
             HttpStatus.BAD_REQUEST.getReasonPhrase(),
             "Validation failed",
@@ -33,4 +35,20 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(DadosIncorretosException.class)
+    public ResponseEntity<ErrorResponseDTO> handleInvalidDataException(DadosIncorretosException ex, HttpServletRequest request) {
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+            LocalDateTime.now().toString(),
+            HttpStatus.BAD_REQUEST.value(),
+            HttpStatus.BAD_REQUEST.getReasonPhrase(),
+            ex.getMessage(),
+            request.getRequestURI());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    public record ErrorResponseWithErrorsMapDTO(String timestamp, int status, String error, String message, String path, Map<String, String> errors) {}
+
+    public record ErrorResponseDTO(String timestamp, int status, String error, String message, String path) {}
 }
