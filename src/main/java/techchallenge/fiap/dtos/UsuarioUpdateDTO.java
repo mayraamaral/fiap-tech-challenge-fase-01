5d4104fd3b6af769897f13687ceec8c8aebecdf5
Mayra Amaral <mayracunha18@gmail.com>
2025-01-12 22:28:30 -0300

    feat: adiciona dto de atualização do usuário

---
+++ src/main/java/techchallenge/fiap/dtos/UsuarioUpdateDTO.java
@@ -0,0 +1,45 @@
+package techchallenge.fiap.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class UsuarioUpdateDTO {
+
+    private DadosAtualizacao dados;
+
+    @NotBlank
+    private String senha;
+
+    @Deprecated
+    public UsuarioUpdateDTO() {}
+
+    public UsuarioUpdateDTO(DadosAtualizacao dados, String senha) {
+        this.dados = dados;
+        this.senha = senha;
+    }
+
+    public DadosAtualizacao getDados() {
+        return dados;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getNome() {
+        return dados.nome();
+    }
+
+    public String getEmail() {
+        return dados.email();
+    }
+
+    public String getLogin() {
+        return dados.login();
+    }
+
+    private record DadosAtualizacao(
+        @NotBlank String nome,
+        @NotBlank String email,
+        @NotBlank String login
+    ) {}
+}
