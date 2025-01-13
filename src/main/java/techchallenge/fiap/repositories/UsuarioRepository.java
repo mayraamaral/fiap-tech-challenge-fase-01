643af031c22c36567db2039fe6df450f88a8abd9
Mayra Amaral <mayracunha18@gmail.com>
2025-01-12 23:02:11 -0300

    chore: adiciona metodo de encontrar usuario pelo login na UsuarioRepository

--- src/main/java/techchallenge/fiap/repositories/UsuarioRepository.java
+++ src/main/java/techchallenge/fiap/repositories/UsuarioRepository.java
@@ -3,5 +3,10 @@ package techchallenge.fiap.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import techchallenge.fiap.entities.Usuario;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    public Optional<Usuario> findByLogin(String login);
+
 }
